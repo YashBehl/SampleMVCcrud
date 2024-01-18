96552bd8df6cb9037be17d4c4a3a65ab67a8c53b
yash.behl <yash.behl@succesive.tech>
2024-01-18 17:44:31 +0530

    Add project files.

---
+++ SampleMVCcrud/Data/SampleDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleMVCcrud.Models.Domain;
+
+namespace SampleMVCcrud.Data
+{
+    public class SampleDbContext : DbContext
+    {
+        public SampleDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ SampleMVCcrud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleMVCcrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
